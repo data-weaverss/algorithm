d02b837d8667d8652b59a5e253563ca6f8e41062
Somin Shim <shimsomin20@gmail.com>
2025-02-24 00:59:33 +0900

    Add: heap problem 1

---
+++ somin/03_heap/01_더맵게.py
@@ -0,0 +1,28 @@
+import heapq
+
+def solution(scoville, K):
+    mix_cnt = 0
+    min_heap = []
+    for s in scoville:
+        heapq.heappush(min_heap, s)
+    
+    # 최댓값이 K 미만이 아닐때까지
+    while min_heap[0] < K:
+        # 모든 음식 스코빌 >= K 가 될 수 없을 때
+        if len(min_heap) == 1: 
+            return -1
+        # 모든 음식 스코빌 >= K
+        first = heapq.heappop(min_heap)
+        second = heapq.heappop(min_heap)
+
+        # 음식 섞고 해당 스코빌 계산
+        heapq.heappush(min_heap, first + second * 2)
+        mix_cnt += 1
+
+    return mix_cnt
+
+if __name__ == "__main__":
+    scoville = [1, 2, 3, 9, 10, 12]
+    K = 7
+    sol = solution(scoville, K)
+    print(sol) # 출력: 2
