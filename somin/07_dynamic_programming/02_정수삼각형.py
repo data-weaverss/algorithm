b4a1502a6ef0fb975f9ca3d3bc4390cda8e1f6fb
Somin Shim <shimsomin20@gmail.com>
2025-03-26 12:16:12 +0900

    Add: dp problem 2,3

--- somin/07_dynamic_programming/02_정수삼각형.py
+++ somin/07_dynamic_programming/02_정수삼각형.py
@@ -18,7 +18,6 @@ def solution(triangle):
                 triangle[i+1][j] += max(triangle[i][j-1], triangle[i][j])
     return max(triangle[-1])
 
-
 if __name__ == "__main__":
     triangle = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]
     print(solution(triangle)) # 30
