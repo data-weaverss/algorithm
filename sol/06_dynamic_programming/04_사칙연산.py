4c60f7520f580993243846f1e6266e94e8833795
sol-commits <vvluc0_2@naver.com>
2025-03-27 13:51:48 +0900

    Add: dp problem 4

--- sol/06_dynamic_programming/04_사칙연산.py
+++ sol/06_dynamic_programming/04_사칙연산.py
@@ -5,35 +5,38 @@ def solution(arr):
     Returns
     - 서로 다른 연산순서의 계산 결과 중 최댓값
     """
-    arr_int = []
-    sign = 1
+    nums = []
+    signs = [1]
     for num in arr:
         if num == "+":
-            sign = 1
+            signs.append(1)
         elif num == "-":
-            sign = -1
+            signs.append(-1)
         else:
-            num = int(num)
-            arr_int.append(num * sign)
+            nums.append(int(num))
     
-    dp = [[0] * len(arr_int) for _ in range(len(arr_int))]
-    for i in range(len(arr_int)):
-        dp[i][i] = arr_int[i]
+    max_dp = [[0] * len(nums) for _ in range(len(nums))]
+    min_dp = [[0] * len(nums) for _ in range(len(nums))]
+    for i in range(len(nums)):
+        max_dp[i][i] = nums[i]
+        min_dp[i][i] = nums[i]
                 
     # bottom up
-    for i in range(len(arr_int) - 2, -1, -1):
-        for j in range(i + 1, len(arr_int)):
-            candidates = []
+    for i in range(len(nums) - 2, -1, -1):
+        for j in range(i + 1, len(nums)):
+            max_val = float('-inf')  
+            min_val = float('inf')
             for k in range(i, j):
-                if dp[i][k] < 0 and i != 0:
-                    candidates.append(-1*(-dp[i][k] + dp[k+1][j]))
-                else:
-                    candidates.append(dp[i][k] + dp[k+1][j])
-            dp[i][j] = max(candidates)
-            print(dp)
-    return dp[0][-1]
+                sign = signs[k+1]
+                a, b, c, d = max_dp[i][k], min_dp[i][k], max_dp[k+1][j], min_dp[k+1][j]
+                candidates = [a + sign*c, a + sign*d, b + sign*c, b + sign*d]
+                max_val = max(max_val, *candidates)
+                min_val = min(min_val, *candidates)
+            max_dp[i][j] = max_val
+            min_dp[i][j] = min_val
+    return max_dp[0][-1]
 
 if __name__ == "__main__":
-    arr = ["10", "-5", "+", "7", "+", "9", "-", "20", "-", "30", "+", "10"]
+    arr = ["1", "-", "3", "+", "5", "-", "8"]
     print(solution(arr)) # 1
     
