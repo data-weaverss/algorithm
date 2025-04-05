0e8be155fc6041bf65a75d2a07d224c6d9007f71
sol-commits <vvluc0_2@naver.com>
2025-04-06 05:35:43 +0900

    Add: dfs/bfs problem 7

--- sol/07_dfs_bfs/07_퍼즐조각채우기.py
+++ sol/07_dfs_bfs/07_퍼즐조각채우기.py
@@ -1,3 +1,12 @@
+"""
+규칙: 한번에 하나씩, 조각 회전 O, 새로 채워 넣은 퍼즐 조각과 인접한 칸이 비어있으면 안됨
+Args
+    game_board: 현재 게임 보드의 상태
+    table: 테이블 위에 놓인 퍼즐 조각의 상태
+Returns
+    규칙에 맞게 최대한 많은 퍼즐 조각을 채워 넣을 경우, 총 몇 칸을 채울 수 있는지
+"""
+
 from collections import defaultdict
 
 def dfs(map, moves, n, cur_row, cur_col, routes):
@@ -8,67 +17,61 @@ def dfs(map, moves, n, cur_row, cur_col, routes):
             if map[new_row][new_col] == 1 and (new_row, new_col) not in routes:
                 dfs(map, moves, n, new_row, new_col, routes)    
 
-def get_puzzle(map, routes, is_game_board=False):
-    # 행과 열의 범위 계산
+def extract_puzzle(map, routes):
+    """주어진 좌표를 기반으로 퍼즐 조각 추출"""
     row1, row2 = min(r[0] for r in routes), max(r[0] for r in routes)
     col1, col2 = min(r[1] for r in routes), max(r[1] for r in routes)
     
-    # 퍼즐 조각 추출 
-    origin_puzzle = [row[col1:col2+1] for row in map[row1:row2+1]]
+    puzzle = [[0] * (col2 - col1 + 1) for _ in range(row2 - row1 + 1)]
+    for row, col in routes:
+        puzzle[row - row1][col - col1] = 1
+        map[row][col] = 0
+    return puzzle
 
-    # 추출된 영역을 0으로 초기화
-    for row in range(row1, row2 + 1):
-        for col in range(col1, col2 + 1):
-            map[row][col] = 0
-    
-    if not is_game_board:
-        puzzle_rotations = [origin_puzzle]
-        for _ in range(1, 4):  # 90도씩 총 3번 회전
-            rotated = [list(x) for x in zip(*puzzle_rotations[-1])][::-1]
-            puzzle_rotations.append(rotated)
-        # 문자열로 변환
-        puzzle_rotations_str = [str(p) for p in puzzle_rotations]
-        return puzzle_rotations_str
+def generate_rotations(puzzle):
+    """퍼즐 조각의 모든 회전(90도씩)을 생성"""
+    rotations = [puzzle]
+    for _ in range(3):  # 총 3번 회전하여 90도씩 추가
+        rotated = [list(x) for x in zip(*rotations[-1])][::-1]
+        rotations.append(rotated)
+    return rotations
 
-    return str(origin_puzzle)
+def normalize_puzzle(puzzle):
+    """퍼즐을 문자열로 변환하여 비교 가능하게 만듦"""
+    return str(puzzle)
     
 def solution(game_board, table):
-    """
-    규칙: 한번에 하나씩, 조각 회전 O, 새로 채워 넣은 퍼즐 조각과 인접한 칸이 비어있으면 안됨
-    Args
-        game_board: 현재 게임 보드의 상태
-        table: 테이블 위에 놓인 퍼즐 조각의 상태
-    Returns
-        규칙에 맞게 최대한 많은 퍼즐 조각을 채워 넣을 경우, 총 몇 칸을 채울 수 있는지
-    """
-    answer = 0
     n = len(table)
     moves = ((-1, 0), (1, 0), (0, 1), (0, -1))
     table_puzzles, board_puzzles = [], []
     
-    for i in range(n): # game_board 0, 1 반전
+    # game_board 에서 빈칸을 퍼즐이라 생각하고,
+    # game_board의 퍼즐과 table의 퍼즐 중 몇개가 일치하는지 찾는 것으로 접근
+    for i in range(n): # game_board 0, 1 반전 -> 빈칸을 퍼즐로 여기도록 반전시킴
         for j in range(n):
             game_board[i][j] = 1 - game_board[i][j]
     
     for row in range(n):
         for col in range(n):
-            if table[row][col] == 1:
+            if table[row][col] == 1:  # 테이블에서 퍼즐 찾기
                 routes = []
                 dfs(table, moves, n, row, col, routes)
-                table_puzzles.append(get_puzzle(table, routes))
-            if game_board[row][col] == 1:
+                puzzle = extract_puzzle(table, routes)
+                table_puzzles.append([normalize_puzzle(rot) for rot in generate_rotations(puzzle)])
+            if game_board[row][col] == 1:  # 게임 보드에서 빈 공간 찾기
                 routes = []
                 dfs(game_board, moves, n, row, col, routes)
-                board_puzzles.append(get_puzzle(game_board, routes, is_game_board=True))
+                puzzle = extract_puzzle(game_board, routes)
+                board_puzzles.append(normalize_puzzle(puzzle))
     
+    # 게임 보드 퍼즐 카운트 저장
     board_puzzles_count = defaultdict(int)
     for board_puzzle in board_puzzles:
         board_puzzles_count[board_puzzle] += 1
     
+    answer = 0
     for puzzle in table_puzzles:
         for rotation in puzzle:
-            print(rotation)
-            print(board_puzzles_count.get(rotation, 0))
             if board_puzzles_count.get(rotation, 0) > 0:
                 answer += rotation.count('1')
                 board_puzzles_count[rotation] -= 1
@@ -87,7 +90,7 @@ if __name__ == "__main__":
     table = [
         [1,0,1,1,1],
         [1,0,0,0,1],
-        [1,0,0,0,1],
+        [1,0,1,0,1],
         [1,0,0,0,1],
         [1,0,0,0,0]
     ]
