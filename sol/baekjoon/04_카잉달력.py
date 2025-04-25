80b3b60a7b7d5824a23ab8a9c7017c7823db6682
sol-commits <vvluc0_2@naver.com>
2025-04-25 13:45:12 +0900

    Add: 백준 6064번 카잉달력

---
+++ sol/baekjoon/04_카잉달력.py
@@ -0,0 +1,36 @@
+import sys
+from math import gcd
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+def solution(M, N, x, y):
+    """
+    <M:N> = 마지막 해, <x:y> = ?
+    """  
+    L = lcm(M, N) # 최소공배수
+    
+    k = x
+    while k <= L:
+        # 기존 방식
+        # cur_y = k % N if k % N != 0 else N
+        # if cur_y == y:
+        #     return k
+        
+        # 개선
+        # k = 2, M = 4, N = 2
+        # (1 % 2) + 1 = 2
+        if (k - 1) % N + 1 == y:
+            return k
+        
+        k += M
+        
+    return -1  
+        
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    T = int(input()) 
+    cases = [list(map(int, input().split())) for _ in range(T)]
+        
+    for case in cases:
+        print(solution(case[0], case[1], case[2], case[3]))
