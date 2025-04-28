f6fdd6cdaa3d6e52841a28759d7400c04b7ca6d6
sol-commits <vvluc0_2@naver.com>
2025-04-28 13:35:40 +0900

    Add: 백준 14500번 테트로미노

--- sol/baekjoon/05_테트로미노.py
+++ sol/baekjoon/05_테트로미노.py
@@ -1,37 +1,54 @@
 import sys
 
+def rotate(shape):
+    """90도 회전"""
+    return [list(row) for row in zip(*shape[::-1])]
+
+def reflect(shape):
+    """좌우 대칭"""
+    return [row[::-1] for row in shape]
+
+def get_all_shapes(shape):
+    shapes = []
+    for _ in range(4):
+        shape = rotate(shape)
+        shapes.append(shape)
+        shapes.append(reflect(shape))
+    # 중복 제거
+    unique = []
+    for s in shapes:
+        if s not in unique:
+            unique.append(s)
+    return unique
+
 def solution(n, m, grid):
     """
     세로 n, 가로 m 
     테트로미노가 놓인 칸에 쓰여 있는 수들의 최댓값 반환
     """  
-    moves = [(1, 0), (0, 1)]
-    totals = []
-    visited = []
-    stack = []
-    for row in range(n):
-        for col in range(m):
-            stack.append([[(row, col)], grid[row][col]])
-    stack = stack[::-1]
-    while stack:
-        routes, total = stack.pop()
-        if routes in visited:
-            continue
-        visited.append(routes)
-        if len(routes) == 4:
-            totals.append((routes, total))
-        else:
-            for drow, dcol in moves:
-                for route in routes:
-                    cur_row, cur_col = route
-                    new_row, new_col = cur_row + drow, cur_col + dcol
-                    if 0 <= new_row < n and 0 <= new_col < m:
-                        if (new_row, new_col) not in routes:
-                            stack.append((set((new_row, new_col)) + routes, total + grid[new_row][new_col]))
-                    
-    totals.sort(key=lambda key: -key[1])
+    tet = [
+        [[1, 1, 1, 1]], 
+        [[1, 1], [1, 1]],
+        [[1, 0], [1, 0], [1, 1]],
+        [[1, 0], [1, 1], [0, 1]],
+        [[1, 1, 1], [0, 1, 0]]
+        ]
     
-    return totals[0][1]
+    answer = 0
+    for te in tet:
+        for t in get_all_shapes(te):
+            w, h = len(t[0]), len(t)
+            for i in range(m):
+                if i + w > m:
+                    break
+                for j in range(n):
+                    if j + h > n:
+                        break
+                    answer = max(
+                        sum(sum(grid[l][k] for l in range(j, j+h, 1) if t[l-j][k-i]) for k in range(i, i+w, 1)), 
+                        answer)
+    return answer
+        
 if __name__ == "__main__":
     input = sys.stdin.readline
     N, M = tuple(map(int, input().split()))
