1248de8fdfe4e6abae9a04728e2ea3c6d5277d43
sol-commits <vvluc0_2@naver.com>
2025-05-03 19:50:42 +0900

    Add: 백준 1062번 가르침 - fix(loop-optimization, data-filtering)

--- sol/baekjoon/09_가르침_timeout.py
+++ sol/baekjoon/09_가르침_timeout.py
@@ -7,7 +7,6 @@ def solution(teach_cnt, words):
     words: anta ** tica 인 남극언어
     """  
     teach_cnt -= 5
-    
     if teach_cnt < 0:
         return 0
     
@@ -15,23 +14,36 @@ def solution(teach_cnt, words):
     answer = 0
     necessary = set('antatica')
     required_letters = set()
+    always_valid = 0
+    word_strip = []
     for word in words:
         filtered = set(word) - necessary
         if not filtered:
+            always_valid += 1
             continue
         elif len(filtered) <= teach_cnt:
             required_letters.update(filtered)
+            word_strip.append(filtered)
+    
+    if len(required_letters) <= teach_cnt:
+        return always_valid + len(word_strip)
     
     # 21개 중 K-5개 선택, C(21, K-5) 최댓값: C(21, 11) = 352716
     for chosen_words in combinations(required_letters, teach_cnt):
         count = 0
-        for word in words: # N: 50
-            # 15 + K-5 + 5
-            if len(set(word) - set(chosen_words) - necessary) == 0:
+        # 21 + 5
+        learned = set(chosen_words) | necessary
+        for word in word_strip: # N: 50
+            # 7
+            for c in word:
+                if c not in learned:
+                    break
+            else:
                 count += 1
+                    
         answer = max(answer, count)
-    # 352,716 × 50 × 15 = 264,537,000
-    return answer
+    # 352,716 × 50 × 7 = 123450600
+    return always_valid + answer
     
 if __name__ == "__main__":
     input = sys.stdin.readline
