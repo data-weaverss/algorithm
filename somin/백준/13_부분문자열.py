94f1af5f26e1c25cc632e781eeb61b0c7e998575
Somin Shim <shimsomin20@gmail.com>
2025-05-12 13:58:40 +0900

    Modify: 부분문자열

--- somin/백준/13_부분문자열.py
+++ somin/백준/13_부분문자열.py
@@ -2,12 +2,8 @@ import sys
 
 def solution(S, P):
     """P가 S의 부분 문자열이면 1, 아니면 0을 출력한다."""
-    n = len(S)
-    m = len(P)
-    for i in range(n - m + 1):
-        if S[i:i+m] == P:
-            return 1
-
+    if P in S:
+        return 1
     return 0
 
 if __name__ == "__main__":
