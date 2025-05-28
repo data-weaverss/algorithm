37ce4ff815d01e1c57e00178c121f2cb36747ca1
sol-commits <vvluc0_2@naver.com>
2025-05-28 15:19:00 +0900

    Modify: 백준 12026번 BOJ 거리 - refactoring
    sort 개선

--- sol/baekjoon/27_boj거리.py
+++ sol/baekjoon/27_boj거리.py
@@ -10,7 +10,7 @@ def solution(block_num, block):
     
     1 <= block_num <= 1,000
     
-    총 시간 복잡도: O(10^6 log10^3)
+    총 시간 복잡도: O(10^6)
     핵심 아이디어: 현재 단계의 가능한 점프 수를 이전 단계의 결과에 의존
     """  
     # dp: 각 블록 종류별로 (인덱스, 누적 점프 에너지) 튜플을 저장하는 딕셔너리
@@ -30,17 +30,16 @@ def solution(block_num, block):
         if prev_char not in dp: continue 
         
         # 이전 블록의 모든 위치에서 현재 위치로 점프하는 경우를 모두 계산
-        candidate_jumps = []
+        min_jump = 0, float('inf')
         for prev_idx, jump_cnt in dp[prev_char]:
             jump_cnt += (idx - prev_idx) ** 2
-            candidate_jumps.append((idx, jump_cnt))
+            if min_jump[1] > jump_cnt:
+                min_jump = idx, jump_cnt
             
-        # 누적 에너지가 가장 작은 경로만 선택 (최적 경로만 남김)
-        candidate_jumps.sort(key = lambda key: key[1])
-        dp[block[idx]].append(candidate_jumps[0])
+        dp[block[idx]].append(min_jump)
         
         if idx == block_num - 1:
-            return candidate_jumps[0][1]
+            return min_jump[1]
         
     return -1
                 
